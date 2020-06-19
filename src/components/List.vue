c80d54e5211a79c5637cf5b43f34ecc22eb8f10e
Luis Angel Larios Castillo <luis44larios.99@gmail.com>
2020-06-18 20:49:20 -0500

    Se instala y configura django-cors-header

---
+++ src/components/List.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div class="box" v-for="e in elements" v-bind:key="e">
+            {{ e }}
+        </div>
+        <button v-on:click="findAll">Click</button>
+        {{ msj }}
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            msj:"... :(",
+            elements: ["Uno", "Dos", "Tres"]
+        };
+    },
+    methods:{
+        findAll: function(){
+           fetch('http://localhost:8000/api/element/?format=json')
+                .then(res => res.json())
+                .then(res => console.log(res))
+
+           
+        }
+    },
+}
+</script>
+
+<style>
+    .box{
+
+        border: 3px solid #CCC;
+        margin: 5px 0 0 0;
+    }
+
+</style>
