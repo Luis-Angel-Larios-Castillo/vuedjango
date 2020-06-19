48997839908e380d70b5f2c85b2426d12ec90348
Luis Angel Larios Castillo <luis44larios.99@gmail.com>
2020-06-19 13:55:02 -0500

    Se instala y configura axios en detail.vue

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -3,6 +3,9 @@
            <b-card>
                <h1>{{ element.title }}</h1>
                <div class="p-3">
+
+                   <router-link class="btn btn-danger btn-sm" :to="{name:'list-category', params:{id: element.category}}">{{category.title}}</router-link>
+                   <router-link class="ml-2 btn btn-danger btn-sm" :to="{name:'list-type', params:{id: element.type}}">{{type.title}}</router-link>
                     <b-card-text>{{ element.description }}</b-card-text>
                </div>
             </b-card>
@@ -10,6 +13,10 @@
 </template>
 
 <script>
+
+const axios = require('axios');
+
+
 export default {
 
     created(){
@@ -21,17 +28,40 @@ export default {
 
     data(){
         return{
-            element: Object
+            element: Object,
+            category:Object,
+            type:Object
+
         };
     },
     methods:{
         find: function(){
            fetch("http://localhost:8000/api/element/"+this.$route.params.id+"/?format=json")
                 .then(res => res.json())
-                .then(res => (this.element = res));
+                .then(res => {
+                    this.element = res;
+                    this.findCategory(this.element.category);
+                    this.findType(this.element.type);
+                });
+
+           
+        },
+         findCategory: function(id){
+           axios.get("http://localhost:8000/api/category/"+id+"/?format=json")
+                //.then(res => res.json())
+                .then(res => (this.category = res.data));
+
+           
+        },
+         findType: function(id){
+           axios.get("http://localhost:8000/api/type/"+id+"/?format=json")
+                //.then(res => res.json())
+                .then(res => (this.type = res.data));
 
            
         }
+
+
     }
 };
 </script>
